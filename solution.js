da44e5d9194008076188bd780d041dd5d54b5f39
Amit Kumar <106420527+amitjait@users.noreply.github.com>
2022-07-11 22:30:35 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -2,5 +2,19 @@
 function major(arr)
 {
   //your code here
+  let hm = new Array();
+  var maxLength = 0;
+  for(var i=0; i<arr.length; i++){
+    hm[arr[i]]++;
+    if(arr[i] > maxLength){
+      maxLength=arr[i];
+    }
+  }
+  const major = arr.length/2;
+  for(var i=0; i<=maxLength; i++){
+    if(hm[i] > major){
+      return hm[i];
+    }
+  }
 }
 module.exports=major;
